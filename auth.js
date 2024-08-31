aee38721e11add83d5ec1cc57f3ae276b4444ca8
AndroGratos <androgratos.apk@gmail.com>
2024-08-31 10:20:06 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
-import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
+// Configuration Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyD2MzAcFOGELXdRwdK-C1Mczm2quyV-HZs",
     authDomain: "generateurtoken-e282f.firebaseapp.com",
@@ -13,18 +15,24 @@ const firebaseConfig = {
 // Initialisation Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const firestore = getFirestore(app);
 
-export function handleSignOut() {
-    signOut(auth).then(() => {
-        window.location.href = 'login.html'; // Redirection vers la page de connexion
-    }).catch((error) => {
-        console.error('Erreur lors de la déconnexion :', error.message);
-    });
-}
-
-export function checkUserStatus() {
-    const user = auth.currentUser;
+// Vérification de l'état de connexion
+onAuthStateChanged(auth, (user) => {
     if (user) {
-        window.location.href = 'index.html'; // Redirection si déjà connecté
+        document.getElementById('welcomeMessage').textContent = 'Bienvenue, ' + user.email;
+    } else {
+        window.location.href = 'login.html'; // Redirection vers la page de connexion
     }
-}
+});
+
+// Fonction de déconnexion
+document.getElementById('logoutButton').addEventListener('click', () => {
+    signOut(auth)
+        .then(() => {
+            window.location.href = 'login.html'; // Redirection vers la page de connexion
+        })
+        .catch((error) => {
+            console.error('Erreur de déconnexion:', error.message);
+        });
+});
